62185599ec47c4e7e56780dab6c9b3059847c492
emerson25k5 <emerson25k5@gmail.com>
2023-11-25 15:42:33 -0300

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "projeto1";
+$database = "swupec92_swupe";
 
 // Conectar ao banco de dados usando MySQLi
 $mysqli = new mysqli($host, $username, $password, $database);
